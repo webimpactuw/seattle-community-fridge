1f2c2e4e1da80ad3933c8ef856926d02f1c40d2f
peterle0604 <petervle@uw.edu>
2024-02-20 00:25:11 -0800

    Create README.md

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Logo from "../assets/SCFLOGO.png";
 import { Link } from 'react-router-dom';
 import ReorderIcon from '@mui/icons-material/Reorder';
 import '../styles/Navbar.css';
+
 function Navbar() {
+
+  const [openLinks, setOpenLinks] = useState(false);
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
+
   return (
     <div className='navbar'>
-      <div className='leftSide'>
+      <div className='leftSide' id={openLinks ? "open" : "close"}>
         <img src={Logo} />
+      <div className='hiddenLinks'>
+        <Link to="/">Home</Link>
+        <Link to="/About Us">About Us</Link>
+        <Link to="/GetInvolved">Get Involved</Link>
+        <Link to="/Volunteer">Volunteer</Link>
+        <Link to="/GetInvolved">Contact Us</Link>
+      </div>
       </div>
       <div className='rightSide'>
         <Link to="/">Home</Link>
@@ -16,7 +30,7 @@ function Navbar() {
         <Link to="/Fridges">Fridges</Link>
         <Link to="/About Us">About Us</Link>
         <Link to="/Donate">Donate</Link>
-        <button>
+        <button onClick={toggleNavbar}>
         <ReorderIcon />
         </button>
       </div>
