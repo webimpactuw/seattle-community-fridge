2be8f1f19542a5670802b34ab4295d619a698cb5
peterle0604 <petervle@uw.edu>
2024-02-22 11:27:56 -0800

    Background image and donate button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,41 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Logo from "../assets/SCFLOGO.png";
 import { Link } from 'react-router-dom';
 import ReorderIcon from '@mui/icons-material/Reorder';
+
 import '../styles/Navbar.css';
 
 function Navbar() {
-
   const [openLinks, setOpenLinks] = useState(false);
+
   const toggleNavbar = () => {
-    setOpenLinks(!openLinks);
+    setOpenLinks(!openLinks)
   };
+
 
   return (
     <div className='navbar'>
       <div className='leftSide' id={openLinks ? "open" : "close"}>
         <img src={Logo} />
-      <div className='hiddenLinks'>
-        <Link to="/">Home</Link>
-        <Link to="/About Us">About Us</Link>
-        <Link to="/GetInvolved">Get Involved</Link>
-        <Link to="/Volunteer">Volunteer</Link>
-        <Link to="/GetInvolved">Contact Us</Link>
-      </div>
+        <div className='hiddenLinks'>
+          <Link to="/">
+            <div>
+              <img src={Logo} alt='Logo' className='Logo'/>
+            </div>
+          </Link>
+          <Link to="/About Us">About Us</Link>
+          <Link to="/GetInvolved">Get Involved</Link>
+          <Link to="/Volunteer">Volunteer</Link>
+          <Link to="/Donate">Contact</Link>      
+        </div>
       </div>
       <div className='rightSide'>
-        <Link to="/">Home</Link>
-        <Link to="/Volunteer">Volunteer</Link>
-        <Link to="/GetInvolved">Get Involved</Link>
-        <Link to="/Fridges">Fridges</Link>
+        <Link to="/"></Link>
         <Link to="/About Us">About Us</Link>
-        <Link to="/Donate">Donate</Link>
+        <Link to="/GetInvolved">Get Involved</Link>
+        <Link to="/Volunteer">Volunteer</Link>
+        <Link to="/Donate">Contact</Link>      
+        {/* <Link to="/Donate">Donate</Link> */}
         <button onClick={toggleNavbar}>
         <ReorderIcon />
         </button>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import BannerImage from '../assets/SeattleProject.png';
+import '../styles/Home.css';
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='headerContainer' style={{ backgroundImage: `url(${BannerImage})` }}>
+        <h1>Seattle Community Fridge</h1>
+        <p>🍅 free food for the community</p>
+        <a href="https://account.venmo.com/u/seattlecommunityfridge" target="_blank">
+        <button>Donate here</button>
+        </a>
+      </div>
+    </div>
   )
 }
 
