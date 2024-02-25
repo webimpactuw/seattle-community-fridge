1617d675810306016b9685a46bcf773079776344
gardnas <134561920+gardnas@users.noreply.github.com>
2024-02-24 16:42:18 -0800

    Created the Get Involved Page
    Started the get involved page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Aboutus from './pages/Aboutus';
+import GetInvolved from './pages/GetInvolved';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <Router>
       <Navbar />
       <Routes>
-      <Route path='/' exact element={<Home />} />  
-      <Route path='About Us' exact element={<Aboutus />}/>    
+        <Route path='/' exact element={<Home />} />  
+        <Route path='About Us' exact element={<Aboutus />}/>
+        <Route path = "/GetInvolved" exact element = {<GetInvolved/>} />
       </Routes>
       </Router>
     </div>
---
+++ src/pages/GetInvolved.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../styles/GetInvolved.css';
+// import BannerImage from '../assets/SeattleProject.png';
+
+function GetInvolved() {
+    return (
+        <div className="get-involved">
+            <h2>How to get involved</h2>
+            <div className="options">
+                <button className="get-involved-button">Food Donations</button>
+                <button className="get-involved-button">Open Roles</button>
+                <button className="get-involved-button">Fridge Donation</button>
+                <button className="get-involved-button">Fridge Hosting</button>
+            </div>
+        </div>
+      )
+    }
+    
+
+export default GetInvolved
