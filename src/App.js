74b9cf02994c09d1379956380d268ad3615ac240
peterle0604 <petervole064@gmail.com>
2024-04-04 22:34:32 -0700

    testing mobile screen size

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <Router>
       <Navbar />
       <Routes>
