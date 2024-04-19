491e8f981332848b23d7a7623f4d56d775cf441d
peterle0604 <petervole064@gmail.com>
2024-04-19 00:59:26 -0700

    about page updated, added sections for photos, about us text, and how it started text and photos

--- src/pages/Aboutus.js
+++ src/pages/Aboutus.js
@@ -1,29 +1,47 @@
-import React from 'react'
+import React from 'react';
 import '../styles/Aboutus.css';
+import AboutImage from '../assets/images.jpg'; // Import your image here
 
 function Aboutus() {
   return (
     <div className="about-us">
-        <h2>About Us</h2>
+      <h2 className='about'>About Us</h2>
+      <div className="about-content">
+        {/* Main content */}
+        <img src={AboutImage} className='about-photo' alt="About" />
         <p className='aboutText'>
           Seattle Community Fridge is a mutual-aid organization providing free 
           food and essentials to communities across the Seattle Metropolitan Area. Started in 
           2020 to target food insecurity during the pandemic, SCF is inspired by the Los Angeles 
           and New York Community Fridges
         </p>
-        <div className="about-options">
-          <button> Get Involved</button>
-          <a href="https://account.venmo.com/u/seattlecommunityfridge" target="_blank" rel="noreferrer">
-            <button>Donate here</button>
-          </a>
-          <button>See Fridges</button>
-        </div>
+      </div>
+      <div className='how-it-started'>
+        <h2 className='how'> How it started</h2>
+
+
+      </div>
+
+      {/* Additional photos */}
+      <div className="additional-photos">
+          <img src={AboutImage} className='photo1'/>
+          <img src={AboutImage} className='photo2'/>
+          <img src={AboutImage} className='photo3'/>
+      </div>
+      <div className='about-bottom'>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit sed risus sed scelerisque. 
+        Etiam sodales maximus neque, sed ornare eros posuere porta. Nunc dapibus dui sed massa aliquet maximus. 
+        Nunc elementum, erat nec suscipit varius, est est lobortis neque, in tempus erat est sit amet lacus. 
+        Donec ipsum eros, vestibulum at massa ut, tempor porttitor nisl. Mauris rhoncus convallis accumsan. 
+        Curabitur rutrum posuere augue, 
+        quis volutpat nunc porttitor eget. Mauris rutrum arcu at ipsum iaculis pharetra. 
+      </p>
+      </div>
+      
+      
     </div>
-  )
+  );
 }
 
-
-
-
-
-export default Aboutus
+export default Aboutus;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -32,9 +32,21 @@ function Home() {
       <div className="map">
         <iframe src="https://www.google.com/maps/d/embed?mid=1yvavCfXWP546_SR677u9HK9mY1irWXJ6&ehbc=2E312F" width="1200" height="600" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title = "map of fridge locations"></iframe>
       </div>
-      <div className='freefood'> 
+      <div className='free-food'> 
+      <h2>Free Food</h2>
         <p className='freefood-text'>
-          
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit sed risus sed scelerisque. Etiam sodales maximus neque, sed ornare eros posuere porta. Nunc dapibus dui sed massa aliquet maximus. 
+          Nunc elementum, erat nec suscipit varius, est est lobortis neque, in tempus erat est sit amet lacus. Donec ipsum eros, vestibulum at massa ut, tempor porttitor nisl. Mauris rhoncus convallis accumsan. 
+          Curabitur rutrum posuere augue, quis volutpat nunc porttitor eget. Mauris rutrum arcu at ipsum iaculis pharetra. 
+        </p>
+      </div>
+
+      <div className='get-involved'>
+        <h2>Get Involved</h2>
+        <p className='get-involved-text'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit sed risus sed scelerisque. Etiam sodales maximus neque, sed ornare eros posuere porta. Nunc dapibus dui sed massa aliquet maximus. 
+          Nunc elementum, erat nec suscipit varius, est est lobortis neque, in tempus erat est sit amet lacus. Donec ipsum eros, vestibulum at massa ut, tempor porttitor nisl. Mauris rhoncus convallis accumsan. 
+          Curabitur rutrum posuere augue, quis volutpat nunc porttitor eget. Mauris rutrum arcu at ipsum iaculis pharetra.
         </p>
       </div>
     </div>
