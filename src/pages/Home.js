39fa0e2a7554ef898298da1a8057871f6d0665d3
peterle0604 <petervole064@gmail.com>
2024-04-21 00:53:03 -0700

    making home page look better, free food and get involved text at the bottom still need more work done.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,25 +2,25 @@ import React from 'react'
 import '../styles/Home.css';
 function Home() {
   return (
-    
     <body>
       <div className="home">
-        <div className="text-above"> 
-          <p><span>Seattle</span> free food comidas gratas 免费食物 無料の食事 ‘ai<br/>
-            <span>Community     </span> đồ ăn miễn phí 無料の食事 đồ ăn miễn phí <br />
-            <span> Fridges</span> nourriture gratuite อาหารฟรี ʻai manuahi đồ ăn
-          </p> 
-          <hr className='text-above-line'/>
-          
+        <div className="text-container">
+          <div className="text-above"> 
+            <p>
+              <span>Seattle</span> free food comidas gratas 免费食物 無料の食事 ‘ai<br/>
+              <span>Community</span> đồ ăn miễn phí 無料の食事 đồ ăn miễn phí <br />
+              <span> Fridges</span> nourriture gratuite อาหารฟรี ʻai manuahi đồ ăn
+            </p> 
+            <hr className='text-above-line'/>
+          </div>
         </div>
         <div className="fridges-section">
           <h2>Our Fridges
-            
-          <span className='fridge-stock'>
+            <span className='fridge-stock'>
               Click on a fridge to see its current stock.
-          </span>
-            </h2> 
-            <hr className='our-fridges-line'/>
+            </span>
+          </h2> 
+          <hr className='our-fridges-line'/>
           <div className="locations-buttons">
             <button>Rainier Valley</button>
             <button>University Heights Center</button>
@@ -31,26 +31,38 @@ function Home() {
           </div>
         </div>
         <div className="map">
-          <iframe src="https://www.google.com/maps/d/embed?mid=1yvavCfXWP546_SR677u9HK9mY1irWXJ6&ehbc=2E312F" width="1200" height="600" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title = "map of fridge locations"></iframe>
+          <iframe className='map-info' src="https://www.google.com/maps/d/embed?mid=1yvavCfXWP546_SR677u9HK9mY1irWXJ6&ehbc=2E312F" width="1200" height="600" style={{border: 2}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title = "map of fridge locations"></iframe>
         </div>
+
         <div className='free-food'> 
-        <h2 className='freefood-header'>Free Food</h2>
-          <p className='freefood-text'>
+        <h2>Free Food
+            <span className='free-food-header'>
+            Take what you need, give what you can.
+            </span>
+          </h2> 
+
+          <p className='free-food-text'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit sed risus sed scelerisque. Etiam sodales maximus neque, sed ornare eros posuere porta. Nunc dapibus dui sed massa aliquet maximus. 
             Nunc elementum, erat nec suscipit varius, est est lobortis neque, in tempus erat est sit amet lacus. Donec ipsum eros, vestibulum at massa ut, tempor porttitor nisl. Mauris rhoncus convallis accumsan. 
-            Curabitur rutrum posuere augue, quis volutpat nunc porttitor eget. Mauris rutrum arcu at ipsum iaculis pharetra. 
+            Curabitur rutrum posuere augue, quis volutpat nunc porttitor eget. Mauris rutrum arcu at ipsum iaculis pharetra.
           </p>
         </div>
 
+        
+
         <div className='get-involved'>
-          <h2>Get Involved</h2>
+        <h2>Get Involved
+            <span className='get-involved-header'>
+            SCF is made possible by you.
+            </span>
+          </h2> 
           <p className='get-involved-text'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit sed risus sed scelerisque. Etiam sodales maximus neque, sed ornare eros posuere porta. Nunc dapibus dui sed massa aliquet maximus. 
             Nunc elementum, erat nec suscipit varius, est est lobortis neque, in tempus erat est sit amet lacus. Donec ipsum eros, vestibulum at massa ut, tempor porttitor nisl. Mauris rhoncus convallis accumsan. 
             Curabitur rutrum posuere augue, quis volutpat nunc porttitor eget. Mauris rutrum arcu at ipsum iaculis pharetra.
           </p>
         </div>
-    </div>
+      </div>
     </body>
   )
 }
