7a39f4f3c893909364febd429f63d7d3b39da057
peterle0604 <petervole064@gmail.com>
2024-04-27 00:40:50 -0700

    made home page fit for different screen sizes, still need more work to be done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,9 +8,9 @@ import '../styles/Navbar.css';
 function Navbar() {
   const [openLinks, setOpenLinks] = useState(false);
 
-  const toggleNavbar = () => {
-    setOpenLinks(!openLinks)
-  };
+  // const toggleNavbar = () => {
+  //   setOpenLinks(!openLinks)
+  // };
 
 
   return (
@@ -21,12 +21,12 @@ function Navbar() {
               <img src={Logo} alt='Logo' className='Logo'/>
             </div>
           </Link>
-          <div className='hiddenLinks'>
-          <Link to="/About Us">About Us</Link>
-          <Link to="/GetInvolved">Get Involved</Link>
-          <Link to="/Volunteer">Volunteer</Link>
-          <Link to="/Contact">Contact</Link>      
-        </div>
+          {/* <div className='hiddenLinks'>
+            <Link to="/About Us">About Us</Link>
+            <Link to="/GetInvolved">Get Involved</Link>
+            <Link to="/Volunteer">Volunteer</Link>
+            <Link to="/Contact">Contact</Link>      
+          </div> */}
       </div>
       <div className='rightSide'>
         <Link to="/"></Link>
@@ -34,9 +34,9 @@ function Navbar() {
         <Link to="/GetInvolved">Get Involved</Link>
         <Link to="/Volunteer">Volunteer</Link>
         <Link to="/Contact">Contact</Link>
-        <button onClick={toggleNavbar}>
-        <ReorderIcon />
-        </button>
+        {/* <button onClick={toggleNavbar}>
+        <ReorderIcon /> */}
+        {/* </button> */}
       </div>
       </div>
   );
