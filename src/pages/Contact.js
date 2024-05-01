86e3c4fb6d1e915ec1514aae341aeae6e83b3192
gardnas <134561920+gardnas@users.noreply.github.com>
2024-05-01 16:21:49 -0700

    Created and styled the Contact Page.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,19 +1,65 @@
-import React from 'react'
-import '../styles/Guidelines.css';
+import React from 'react';
+import '../styles/Contact.css';
+import emailIcon from '../assets/mail.png';
+import phoneIcon from '../assets/phone.png';
+import socialIcon from '../assets/social.png';
 
 function Contact() {
     return (
-        <div className="get-involved">
-            <h2>How to get involved</h2>
-            <div className="options">
-                <button className="get-involved-button">Food Donations</button>
-                <button className="get-involved-button">Open Roles</button>
-                <button className="get-involved-button">Fridge Donation</button>
-                <button className="get-involved-button">Fridge Hosting</button>
+        <div className="contact-container">
+            <h2>Get in touch</h2>
+            <div className="contact-content">
+                <form className="contact-form">
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" id="name" name="name" />
+                        <div className="line"></div>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" />
+                        <div className="line"></div>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="message">Message</label>
+                        <textarea id="message" name="message"></textarea>
+                        <div className="line"></div>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+                <div className="contact-info">
+                    <div className="info-item">
+                        <img src={emailIcon} alt="Email" />
+                        <div>
+                            <p>Email</p>
+                            <a className="contact-detail" href="mailto:seattlecommunityfridge@gmail.com" target="_blank" rel="noopener noreferrer">
+                              seattlecommunityfridge@gmail.com
+                            </a>
+                        </div>
+                    </div>
+                    <div className="info-item">
+                        <img src={phoneIcon} alt="Phone" />
+                        <div>
+                            <p>Phone</p>
+                            <p className="contact-detail">206</p>
+                        </div>
+                    </div>
+                    <div className="info-item">
+                        <img src={socialIcon} alt="Social" />
+                        <div>
+                            <p>Social</p>
+                            <a className="contact-detail" href="https://www.facebook.com/SeattleCommunityFridge/" target="_blank" rel="noopener noreferrer">
+                            Facebook
+                            </a>
+                            <a className="contact-detail" href="https://www.instagram.com/seattlecommunityfridge/?hl=en" target="_blank" rel="noopener noreferrer">
+                              Instagram
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-      )
+    );
 }
-    
 
-export default Contact
+export default Contact;
