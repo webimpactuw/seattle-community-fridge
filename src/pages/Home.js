b1a158f5f0fda7f29e6ef4312691bfc61d43439c
peterle0604 <petervole064@gmail.com>
2024-05-08 00:42:13 -0700

    homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ function Home() {
   return (
     <body className='homePage'>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-      <div className='homePage'>
+      <div className='home'>
         <div className="text-container">
           <div className="text-above"> 
             <p>
